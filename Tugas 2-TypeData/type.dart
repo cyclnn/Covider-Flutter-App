cf3c2ab3e483a582a4a504232649ff284f8b1943
Irfan Akbari Habibi <irfan.akbarihabibi@gmail.com>
2021-04-27 09:50:24 +0700

    Pengumpulan Tugas 2

---
+++ Tugas 2-TypeData/type.dart
@@ -0,0 +1,73 @@
+// void main(List<String> args) {
+//   var word = "Dart";
+//   var second = "is";
+//   var third = "awesome";
+//   var fourth = "and";
+//   var fifth = "I";
+//   var sixth = "love";
+//   var seventh = "it!";
+
+//   print("$word $second $third $fourth $fifth $sixth $seventh");
+// }
+
+// void main(List<String> args) {
+//   var sentence = "I am going to be Flutter Developer";
+
+//   var firstword = sentence[0];
+//   var secondword = sentence[2] + sentence[3];
+//   var thirdword =
+//       sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9];
+//   var fourthword = sentence[11] + sentence[12];
+//   var fifthword = sentence[14] + sentence[15];
+//   var sixthword = sentence[17] +
+//       sentence[18] +
+//       sentence[19] +
+//       sentence[20] +
+//       sentence[21] +
+//       sentence[22] +
+//       sentence[23];
+//   var seventhword = sentence[25] +
+//       sentence[26] +
+//       sentence[27] +
+//       sentence[28] +
+//       sentence[29] +
+//       sentence[30] +
+//       sentence[31] +
+//       sentence[32] +
+//       sentence[33];
+
+//   print("Firts Word: $firstword");
+//   print("Second Word: $secondword");
+//   print("Third Word: $thirdword");
+//   print("Fourth Word: $fourthword");
+//   print("Fifth Word: $fifthword");
+//   print("Sixth Word: $sixthword");
+//   print("Seventh Word: $seventhword");
+// }
+
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   print("Masukkan Nama Depan: ");
+//   String namdep = stdin.readLineSync()!;
+//   print("Masukkan Nama Belakang: ");
+//   String? nambel = stdin.readLineSync();
+
+//   print("Nama Lengkap Anda Adalah: ${namdep} ${nambel}");
+// }
+
+// void main(List<String> args) {
+//   int a = 5;
+//   int b = 10;
+//   int hasil;
+//   double hasilbagi;
+
+//   hasil = a * b;
+//   print("Perkalian : $hasil");
+//   hasilbagi = a / b;
+//   print("Pembagian : $hasilbagi");
+//   hasil = a + b;
+//   print("Penjumlahan : $hasil");
+//   hasil = a - b;
+//   print("Pengurangan : $hasil");
+// }
