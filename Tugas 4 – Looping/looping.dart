f0e23c4defb2ad3e75fcaba82d15fd6bcb30a613
Irfan Akbari Habibi <irfan.akbarihabibi@gmail.com>
2021-04-29 16:25:19 +0700

    Pengumpulan Tugas 4

---
+++ Tugas 4 – Looping/looping.dart
@@ -0,0 +1,49 @@
+// void main() {
+//   var i = 2;
+//   print("Looping Pertama");
+//   while (i <= 20) {
+//     print("$i - I Love Coding");
+//     i = i + 2;
+//   }
+//   i = i - 2;
+//   print("Looping Kedua");
+//   while (i >= 2) {
+//     print("$i - I Will Become a Mobile Developer");
+//     i = i - 2;
+//   }
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   for (var j = 1; j <= 4; j++) {
+//     for (var i = 1; i <= 8; i++) {
+//     stdout.write("#");
+//     }
+//     print("");
+//   }
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   for (var i = 1; i <= 8; i++) {
+//     for (var j = 1; j < i; j++) {
+//       stdout.write("#");
+//     }
+//     print("");
+//   }
+// }
+
+// void main() {
+//   for (var i = 1; i <= 20; i++) {
+//     var k = i % 2;
+//     if (i % 3 != 0 && k == 1) {
+//       print("$i - Santai");
+//     } else if ( i % 3 == 0 && k == 1) {
+//       print("$i - I Love Coding");
+//     } else if (k == 0){
+//       print("$i - Berkualitas");
+//     }
+//   }
+// }
