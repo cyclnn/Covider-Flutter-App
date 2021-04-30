8e3fe1a8f8119c6294218a7f8f07f770f7fc2ee1
Irfan Akbari Habibi <irfan.akbarihabibi@gmail.com>
2021-04-30 17:20:31 +0700

    Pengumpulan Tugas 5

---
+++ Tugas 5 – Function/function.dart
@@ -0,0 +1,49 @@
+// void main() {
+//   print(teriak());
+// }
+
+// teriak() {
+//   return "Halo Sanbers!";
+// }
+
+// void main() {
+//   var num1 = 12;
+//   var num2 = 4;
+//   var hasil = kalikan(num1,num2);
+
+//   print(hasil);
+// }
+
+// kalikan(num1, num2) {
+//   return num1 * num2;
+// }
+
+// void main() {
+//   var name = "Irfan";
+//   var age = 18;
+//   var address = "Medan";
+//   var hobby = "Baca Komik";
+
+//   print(introduce(name,age,address,hobby));
+// }
+
+// introduce(name, age, address, hobby) {
+//   return "Nama Saya $name, Umur Saya $age Tahun, Alamat Saya di $address, dan Saya Punya Hobi Yaitu $hobby";
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Masukkan Nilai = ");
+//   int x = int.parse(stdin.readLineSync()!);
+//   print(faktorial(x));
+// }
+
+// faktorial(x) {
+//   if (x <= 1) {
+//     return 1;
+//   } else {
+//     var f = x * faktorial(x - 1);
+//     return f;
+//   }
+// }
